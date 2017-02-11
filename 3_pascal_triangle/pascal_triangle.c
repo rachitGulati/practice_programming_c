b6c1b3ab452c364bb49e894f251b29cdf3fdd23a
rachitGulati <rachitgulati26@gmail.com>
2017-02-11 17:21:07 +0530

    Added some code indendation

--- 3_pascal_triangle/pascal_triangle.c
+++ 3_pascal_triangle/pascal_triangle.c
@@ -17,21 +17,20 @@ int main() {
 	int height;
 	printf("Height of Pascal Triangle: ");
 	scanf("%d", &height);
- 
- int *pt[height]; //Array of pointers to point to the dynamic column of each pascal triangle row.
-	for(int i=1; i<=height; i++) {
+	int *pt[height]; //Array of pointers to point to the dynamic column of each pascal triangle row.
+	for(int i = 1; i <= height; i++) {
 		pt[i-1] = (int *)malloc(i* sizeof(int)); //Allocate memeory to each column dynamically
 	}
-	for(int i =0; i< height; i++) { 
+	for(int i = 0; i < height; i++) { 
 		pt[i][0] = 1; //First value in column is always 1
-		for(int j=1; j<i; j++) {
+		for(int j = 1; j < i; j++) {
 			pt[i][j] = pt[i-1][j-1] + pt[i-1][j]; //Compute value based on last row of triangle
 		}
 		pt[i][i] = 1; //Last value in column is always 1
 	}
-	for(int i =0; i< height; i++) { 
+	for(int i = 0; i < height; i++) { 
 		for(int k = height - i -1; k > 0; k--) printf(" "); //Space Loop 
-		for(int j=0; j<=i; j++) {
+		for(int j = 0; j <= i; j++) {
 			if(i != height) printf(" "); //To manage a better view in console
 			printf("%d",pt[i][j]);
 		}
--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 	- [Fibonacci series](2_fibonacci_series/fibonacci_series.c)
 	- [Fibonacci series with recursion](2_fibonacci_series/fibonacci_series_recursion.c)
 	- [Fibonacci series with memorization](2_fibonacci_series/fibonacci_series_memorize.c)
-	- [Pascal Trianle](3_pascal_triangle/pascal_triangle.c)
+	- [Pascal Triangle](3_pascal_triangle/pascal_triangle.c)
 
